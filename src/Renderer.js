93340957f0440de2e292e38c8e8bc547acce139a
Sabfes <bitaev.db@gmail.com>
2022-10-16 22:14:33 +0300

    t1

--- src/Renderer.js
+++ src/Renderer.js
@@ -26,17 +26,17 @@ export default class Renderer {
   update(time) {
     time *= 0.001
 
-    // this.experience.box.rotation.x = time
-    // this.experience.box.rotation.z = time
-    //
-    // this.experience.box.translateZ(.01)
-    // this.experience.box.rotation.y += .01
-    // this.camera.perspectiveCamera.translateZ(.001)
-    // this.camera.perspectiveCamera.rotation.y += .001
-    // console.log(this.experience)
-    // this.experience.rayCaster.setFromCamera(this.experience.mousePosition, this.camera.perspectiveCamera)
-    // const intersects = this.experience.rayCaster.intersectObjects(this.scene.children)
-    // console.log(intersects)
+    this.experience.box.rotation.x = time
+    this.experience.box.rotation.z = time
+
+    this.experience.box.translateZ(.01)
+    this.experience.box.rotation.y += .01
+    this.camera.perspectiveCamera.translateZ(.001)
+    this.camera.perspectiveCamera.rotation.y += .001
+    console.log(this.experience)
+    this.experience.rayCaster.setFromCamera(this.experience.mousePosition, this.camera.perspectiveCamera)
+    const intersects = this.experience.rayCaster.intersectObjects(this.scene.children)
+    console.log(intersects)
     this.renderer.render(this.scene, this.camera.perspectiveCamera)
     window.requestAnimationFrame(() => this.update(new Date()))
   }
